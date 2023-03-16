b59bc918f24163e58a3d6a0c3c720e29087edf77
Prabhu-Manchineella_bcgovent <prabhu.manchineella@gov.bc.ca>
2023-03-16 14:38:10 -0400

    update script

--- script.py
+++ script.py
@@ -13,21 +13,17 @@ token = os.getenv("token")
 org_name= "prabhukiran9999"
 repo_name = "pr-script"
 repo_url = f'https://{token}@github.com/{org_name}/{repo_name}.git'
-
 repo_dir = os.path.join(os.getcwd(), repo_name)
 os.makedirs(repo_dir, exist_ok=True)
 
 # Clone the repository
 git.Repo.clone_from(repo_url, repo_dir, recursive=True)
 os.chdir(repo_name)
-pwd = os.getcwd()
-print (pwd)
 repo_path = "./"
 repo = Repo(repo_path)
 
 #Get GH Cli version
 gh_version = call(["gh", "--version"])
-
 
 # Execute the `gh auth login` command and provide your personal access token as input
 subprocess.run(["gh", "auth", "login", "--with-token"], input=f"{token}\n", text=True)
@@ -40,7 +36,7 @@ admin_email = project_data["project_set_info"]["admin_email"]
 admin_name = project_data["project_set_info"]["admin_name"]
 billing_group = project_data["project_set_info"]["billing_group"] 
 # Look for a specific key in the JSON object
-key="licence_Plate"
+key="licence_plate"
 LicencePlate = project_data["project_set_info"].get(key)
 # Execute project-set creation script
 if LicencePlate:
@@ -61,8 +57,8 @@ else:
         print(e)
 os.chdir('../')
 # Get the project-set name created
-project_Set_info = subprocess.Popen(["git", "ls-files", "--others", "--directory", "--exclude-standard"],stdout=subprocess.PIPE).communicate()[0].decode("utf-8").rstrip()
-checkout_branch_name = project_Set_info.strip("/").replace("projects/", "")
+project_set_info = subprocess.check_output(["git", "ls-files", "--others", "--directory", "--exclude-standard"], cwd=repo_path).decode("utf-8").rstrip()
+checkout_branch_name = project_set_info.strip("/").replace("projects/", "")
 print(checkout_branch_name)
 
 
@@ -71,11 +67,11 @@ checkout_branch = repo.git.checkout('-b', checkout_branch_name)
 # Commit message for Push
 COMMIT_MESSAGE = f'Creating accounts for new project set-{checkout_branch_name}'
 def git_push():
-    branch = checkout_branch_name
+    # branch = checkout_branch_name
     repo = Repo('.')
     repo.git.add(all=True)
     repo.index.commit(COMMIT_MESSAGE)
-    remote = repo.remote(name="origin")
+    # remote = repo.remote(name="origin")
     origin = repo.remote(name='origin')
     origin.push(checkout_branch_name)
     
@@ -84,17 +80,16 @@ time.sleep(5)
 
 
 ## Create Pull reequest and sleep for 5 min
-create_pr = subprocess.Popen(["gh", "pr", "create", f"-t Creating accounts for a new project set-{checkout_branch_name}", f"-b creating accounts for a new project set-({checkout_branch_name}) using provisonor script", "-rwrnu"],stdout=subprocess.PIPE).communicate()[0] 
-pr_url = create_pr.decode("utf-8").rstrip() 
+pr_url = subprocess.check_output(["gh", "pr", "create", f"-t Creating accounts for a new project set-{checkout_branch_name}", f"-b creating accounts for a new project set-({checkout_branch_name}) using provisonor script", "-rwrnu"]).decode("utf-8").rstrip()
 print (f'Pull_request for new project-set({checkout_branch_name}) accounts created successfully')
 #Sleep for 5 sec after pull request is created so the actions will register
 time.sleep(5) #Sleep for 5 secs
 print(pr_url)
 
 # Check for pull request actions to complete
-check_pr = json.loads(subprocess.Popen(["gh", "pr", "view", pr_url, "--json", "statusCheckRollup"],stdout=subprocess.PIPE).communicate()[0].decode("utf-8").rstrip())
+check_pr = json.loads(subprocess.check_output(["gh", "pr", "view", pr_url, "--json", "statusCheckRollup"]).decode("utf-8").rstrip())
 print(check_pr)
-workflow_id = str(json.loads(subprocess.Popen(["gh", "run", "list", "-b", checkout_branch_name, "-L", "1", "--json", "databaseId"],stdout=subprocess.PIPE).communicate()[0])[0]['databaseId'])
+workflow_id = str(json.loads(subprocess.check_output(["gh", "run", "list", "-b", checkout_branch_name, "-L", "1", "--json", "databaseId"]))[0]['databaseId'])
 step = "account-creation"
 
 # Function to get the pull request workflow status and merge the PR once the workflow status are successful
@@ -102,13 +97,10 @@ def pr_workflow_status(workflow_id,pr_url):
     workflow_status = ""
     while workflow_status != "completed":
         time.sleep(5)
-        workflow_status = json.loads(subprocess.Popen(["gh", "run", "view", workflow_id, "--json", "status"],stdout=subprocess.PIPE).communicate()[0])['status']
-        workflow_conclusion = json.loads(subprocess.Popen(["gh", "run", "view", workflow_id, "--json", "conclusion"],stdout=subprocess.PIPE).communicate()[0])['conclusion']
-        if workflow_status =='queued':
-            print(f'pull request workflow status for {step} is {workflow_status}')
-            continue
-        elif workflow_status == "in_progress":
-            print(f'pull request workflow status for {step} is {workflow_status}')
+        workflow_status = json.loads(subprocess.check_output(["gh", "run", "view", workflow_id, "--json", "status"]))['status']
+        workflow_conclusion = json.loads(subprocess.check_output(["gh", "run", "view", workflow_id, "--json", "conclusion"]))['conclusion']
+        if workflow_status in ('queued', 'in_progress'):
+            print(f'Pull request workflow status for {step} is {workflow_status}')
             continue
         elif workflow_status == "completed" and workflow_conclusion == "success":
             print(f'pull request workflow status for {step} is {workflow_status} and is {workflow_conclusion}')
@@ -124,20 +116,15 @@ def pr_workflow_status(workflow_id,pr_url):
         elif workflow_status =="completed" and workflow_conclusion == "failure":
             print(f"pull request  workflow for {step} failed")
             return workflow_conclusion
-            break
 
-####To do add some delay for the runner to stop and start
 # Function to get the Push workflow status
 def push_workflow_status(push_workflow_id):
     push_workflow_status = ""
     while push_workflow_status != "completed":
         time.sleep(5)
-        push_workflow_status = json.loads(subprocess.Popen(["gh", "run", "view", push_workflow_id, "--json", "status"],stdout=subprocess.PIPE).communicate()[0])['status']
-        push_workflow_conclusion = json.loads(subprocess.Popen(["gh", "run", "view", push_workflow_id, "--json", "conclusion"],stdout=subprocess.PIPE).communicate()[0])['conclusion']
-        if push_workflow_status =='queued':
-            print(f'push workflow status for {step} is {push_workflow_status}')
-            continue
-        elif push_workflow_status == "in_progress":
+        push_workflow_status = json.loads(subprocess.check_output(["gh", "run", "view", push_workflow_id, "--json", "status"]))['status']
+        push_workflow_conclusion = json.loads(subprocess.check_output(["gh", "run", "view", push_workflow_id, "--json", "conclusion"]))['conclusion']
+        if push_workflow_status in ('queued', 'in_progress'):
             print(f'push workflow status for {step} is {push_workflow_status}')
             continue
         elif push_workflow_status == "completed" and push_workflow_conclusion == "success":
@@ -147,10 +134,9 @@ def push_workflow_status(push_workflow_id):
             print(f"Push workflow for {step} failed")
             return push_workflow_conclusion
 
-
 pr_workflow_status(workflow_id,pr_url)
 time.sleep(5)
-push_workflow_id = str(json.loads(subprocess.Popen(["gh", "run", "list", "-b", "main", "-L", "1", "--json", "databaseId"],stdout=subprocess.PIPE).communicate()[0])[0]['databaseId'])
+push_workflow_id = str(json.loads(subprocess.check_output(["gh", "run", "list", "-b", "main", "-L", "1", "--json", "databaseId"]))[0]['databaseId'])
 push_status = push_workflow_status(push_workflow_id)
 print(push_status)
 
@@ -172,7 +158,7 @@ os.chdir('../')
 COMMIT_MESSAGE = f'Creating other layers for the project set-{checkout_branch_name}'
 git_push()
 step = "layer-creation"
-layer_pr = subprocess.Popen(["gh", "pr", "create", f"-t Adding Layers to the project set-{checkout_branch_name}", f"-b Adding layers to the new project set-{checkout_branch_name} using provisonor script", "-rwrnu"],stdout=subprocess.PIPE).communicate()[0] 
+layer_pr = subprocess.check_output(["gh", "pr", "create", f"-t Adding Layers to the project set-{checkout_branch_name}", f"-b Adding layers to the new project set-{checkout_branch_name} using provisonor script", "-rwrnu"])
 pr_url = layer_pr.decode("utf-8").rstrip() 
 print ('Pull_request for layers created successfully')
 
@@ -180,10 +166,10 @@ print ('Pull_request for layers created successfully')
 #Sleep for 5 sec after pull request is created so the actions will register
 time.sleep(5) #Sleep for 5 secs
 print(pr_url)
-workflow_id = str(json.loads(subprocess.Popen(["gh", "run", "list", "-b", checkout_branch_name, "-L", "1", "--json", "databaseId"],stdout=subprocess.PIPE).communicate()[0])[0]['databaseId'])
+workflow_id = str(json.loads(subprocess.check_output(["gh", "run", "list", "-b", checkout_branch_name, "-L", "1", "--json", "databaseId"]))[0]['databaseId'])
 pr_workflow_status(workflow_id,pr_url)
 time.sleep(5)
-push_workflow_id = str(json.loads(subprocess.Popen(["gh", "run", "list", "-b", "main", "-L", "1", "--json", "databaseId"],stdout=subprocess.PIPE).communicate()[0])[0]['databaseId'])
+push_workflow_id = str(json.loads(subprocess.check_output(["gh", "run", "list", "-b", "main", "-L", "1", "--json", "databaseId"]))[0]['databaseId'])
 push_status = push_workflow_status(push_workflow_id)
 print(push_status)
 
